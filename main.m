055749a8b461717d7e6f9ba38b7142031a35b02e
Bonaventura <btagliafierro@gmail.com>
2021-05-17 14:42:06 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,22 +1,37 @@
 clear;close all; clc
 
-  %%%% load ground motion - here an old one from a txt
+  
+  g=9.81;
   damping=0.05;
+  period=0.85;
+  
+  %%%% load ground motion - here an old one from a .dat
+  temp = load('Kobe95.dat');
+  time=temp(:,1);
+  signal=temp(:,2);
 
   
-target=makeSpectrum();
-target.damping=damping ;
-target.time=time;
-target.signal=acceleration;
+spectrum=makeSpectrum();
+spectrum.damping=damping ;
+spectrum.time=time;
+spectrum.signal=signal;
 
-targetSpectrum=target.spectrum;
-periodSpectrum=target.period; 
-
-
+spectrumSpa=target.spectrum;
+spectrumPeriod=target.period; 
 
 
 %%% look for the wanted pseudo acceleartion for your calculation
   
-index=find(periodSpectrum>=period,1);
-Spa=targetSpectrum(index);
-disp(['questo è il valore di Spa ' num2str(Spa)])
+index=find(spectrumPeriod>=period,1);
+Spa=spectrumSpa(index);
+disp([Spa at ' num2str(period) 's is ' num2str(Spa/g) 'g'])
+
+
+%%% plot
+plot(spectrumPeriod,spectrumSpa/g*,'r--')
+plot(period,Spa/g*,'.')
+
+axis([0 4 0 inf])
+legend({'elastic spectrum','selected period'})
+xlabel('Period [s]')
+ylabel('Sa [g]')
